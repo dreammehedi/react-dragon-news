a5d40362c581c782b77fc2ea46b98109c14cf69f
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-04 10:21:08 +0600

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,sx}"],
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        "dark-2": "#403F3F",
+        "dark-3": "#706F6F",
+        "dark-4": "#9F9F9F",
+        "dark-6": "#E7E7E7",
+        "dark-7": "#F3F3F3",
+      },
+    },
+  },
+  plugins: [],
+};
